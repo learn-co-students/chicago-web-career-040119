9d7890eb735b529d38cadd1a2f3b529695251cc3
NoahHeinrich <noawriteplay@gmail.com>
2019-06-05 13:02:33 -0500

    added lecture code

---
+++ 32-thinking-in-react/src/components/Pet.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+class Pet extends React.Component {
+
+  handleClick = () => {
+    this.props.onAdoptPet(this.props.pet.id)
+  }
+  render() {
+    return (
+      <div className="card">
+        <div className="content">
+          <a className="header">
+            {/*'♀' OR '♂' */}
+            {this.props.pet.gender === "male" ? '♂' : '♀'}
+            {this.props.pet.name}
+          </a>
+          <div className="meta">
+            <span className="date">{this.props.pet.type}</span>
+          </div>
+          <div className="description">
+            <p>Age: {this.props.pet.age}</p>
+            <p>Weight: {this.props.pet.weight}</p>
+          </div>
+        </div>
+        <div className="extra content">
+          {this.props.pet.isAdopted ? <button className="ui disabled button">Already adopted</button>
+          : <button onClick={this.handleClick} className="ui primary button">Adopt pet</button>}
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Pet
